39394cf16989eec68da94b0e6942147f7f0514da
Anboz Davlatkadamov <anboz.d@gmail.com>
2020-11-06 23:35:14 +0500

    Solved the homeworks exercise 2 from class_1

---
+++ lesson_11/problem_2/src/com/company/another/Variables.java
@@ -0,0 +1,11 @@
+package com.company.another;
+
+public class Variables {
+    public int x;
+    public int y;
+
+    public Variables(int xFromConstructor, int yFromConstructor){
+        x=xFromConstructor;
+        y=yFromConstructor;
+    }
+}
---
+++ lesson_11/problem_2/src/com/company/main/Main.java
@@ -0,0 +1,11 @@
+package com.company.main;
+
+import com.company.another.Variables;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Variables variables = new Variables(10,20);
+    }
+}
