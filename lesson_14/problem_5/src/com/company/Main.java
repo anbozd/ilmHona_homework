5c9bae6d3313cc0471de2eb935b166720838fb8c
Anboz Davlatkadamov <anboz.d@gmail.com>
2020-11-18 17:52:08 +0500

    Solved the fifth problem from lesson_14 homeworks exersice

---
+++ lesson_14/problem_5/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString = "My letter consist of fifty words. I used in my letter academic word";
+        myString = myString.replace("word", "letter");
+        System.out.print(myString);
+	// write your code here
+    }
+}
